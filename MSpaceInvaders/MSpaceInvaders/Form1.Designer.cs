2664369bcb2352eb5d68a6a79c2f1e8cdbb592c9
janestojilkov <janestojilkov@yahoo.com>
2019-06-17 17:21:57 +0200

    Test

--- MSpaceInvaders/MSpaceInvaders/Form1.Designer.cs
+++ MSpaceInvaders/MSpaceInvaders/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- MSpaceInvaders/MSpaceInvaders/Form1.cs
+++ MSpaceInvaders/MSpaceInvaders/Form1.cs
@@ -16,5 +16,10 @@ namespace MSpaceInvaders
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Comment
+        }
     }
 }
