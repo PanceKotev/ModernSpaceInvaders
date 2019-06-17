1021ca1a8c7cdcb81483d7ea688500755c528385
Панче Котев <Panche.Kotev@students.finki.ukim.mk>
2019-06-17 21:16:59 +0200

    Added Game

---
+++ MSpaceInvaders/MSpaceInvaders/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSpaceInvaders
+{
+    public class Game
+    {
+        public List<Projectile> firedP { get; set; }
+
+    }
+}
